f48f1927e7c11b758ad2813463047da260e52d0e
max <max.pervukhin@gmail.com>
2017-03-10 02:18:03 +0700

    init

---
+++ Assets/Plane.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Plane : MonoBehaviour {
+
+    public Transform target;
+    public Transform target2;
+
+    float speed = 20;
+    float turn = .3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        Vector3 fwd = transform.localToWorldMatrix * Vector3.forward;
+        transform.position += fwd * speed * Time.fixedDeltaTime;
+        Vector3 localTarget = (target.position - transform.position);
+        
+        Vector3 _up = Quaternion.AngleAxis(20,localTarget) * Vector3.up;
+        Vector3 up = transform.worldToLocalMatrix * _up;
+        Quaternion t = Quaternion.LookRotation(localTarget,up);
+        
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, t, Time.fixedDeltaTime);
+
+      //  target2.position = target.position + _up * 20;
+
+      //  EditorApplication.isPaused = true;
+    }
+
+}
